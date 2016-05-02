a7c9d78434cb5ac852f2ea40232a6fc245e57205
Gavin Mendel-Gleason <jacobian@gmail.com>
2016-05-02 22:03:59 +0100

    Adding passgen

--- python/cleanSpreadsheet.py
+++ python/cleanSpreadsheet.py
@@ -2,14 +2,28 @@
 
 import csv
 
-def run(filename):
+def run():
+
+    usernames = {}
+    with open('ngo-usernames.csv', 'rb') as h:
+        names = csv.reader(h, delimiter=',', quotechar="\"")
+        for (name,) in names: 
+            usernames[name] = True
+
+    def userType(name):
+        if name in usernames: 
+            return "Organisation"
+        else:
+            return "Volunteer"
+
     with open('Test.csv', 'wb') as g: 
-        with open(filename, 'rb') as f:        
+        with open('2015 Reviews.xlsx - 160325 Reviews.csv', 'rb') as f:        
             r = csv.reader(f, delimiter=',', quotechar="\"")
             w = csv.writer(g, delimiter=',', quotechar="\"")
 
-            # Write field names
-            # w.writerow(r.fieldnames)
+            # Read / write headers
+            row = r.next()
+            w.writerow(row + ["User Type"])
             
             lastrow = False
             for row in r:
@@ -17,11 +31,13 @@ def run(filename):
                 if (projectid in ['','null'] and comment in ['','null']): 
                     pass
                 elif (projectid in ['','null'] and lastrow):
-                    lastrow = (lastrow[0],lastrow[1],lastrow[2],lastrow[3],lastrow[4],lastrow[5],lastrow[6]+' '+comment,lastrow[7])
+                    lastrow = [lastrow[0],lastrow[1],lastrow[2],lastrow[3],lastrow[4],lastrow[5],lastrow[6]+' '+comment,lastrow[7]]
                 elif lastrow:
-                    w.writerow(lastrow)
+                    w.writerow(lastrow + [userType(lastrow[7])])
                     lastrow = row
                 else:
                     lastrow = row
 
-                    
+if __name__ == "__main__":
+    run()
+    
